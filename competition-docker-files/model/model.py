ae8f0a2a86652924185d7a22828ce2febaf40973
Matheus Centa <matheuscenta@gmail.com>
2019-12-02 19:54:04 +0100

    Disable device log

--- competition-docker-files/model/model.py
+++ competition-docker-files/model/model.py
@@ -24,7 +24,7 @@ from keras.preprocessing import sequence
 from keras.backend.tensorflow_backend import set_session
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True  # dynamically grow the memory used on the GPU
-config.log_device_placement = True  # to log device placement (on which device the operation ran)
+config.log_device_placement = False # to log device placement (on which device the operation ran)
                                     # (nothing gets printed in Jupyter, only if you run it standalone)
 sess = tf.Session(config=config)
 set_session(sess)  # set this TensorFlow session as the default session for Keras
