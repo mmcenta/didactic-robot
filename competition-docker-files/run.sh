7766289a1f0dd2a877074ff5861c7c59aa7aaf0b
Matheus Centa <matheuscenta@gmail.com>
2019-12-02 20:50:13 +0100

    Change run.sh to run on dataset 3

--- competition-docker-files/run.sh
+++ competition-docker-files/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 pip install --upgrade tensorflow-hub
-python run_local_test.py -dataset_dir=./datasets/2 -code_dir=./model
+python run_local_test.py -dataset_dir=./datasets/3 -code_dir=./model
